259d781de3fd3d5ab6b62079f82f31f8d68f89eb
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-08-23 21:34:52 +0800

    品牌商个人信息管理

--- server/src/main/java/skk/controller/MvoController.java
+++ server/src/main/java/skk/controller/MvoController.java
@@ -3,8 +3,10 @@ package skk.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import skk.entity.BrandExtraInfo;
+import skk.entity.BrandInfo;
 import skk.entity.User;
 import skk.repository.BrandExtraInfoRepository;
+import skk.repository.BrandInfoRepository;
 import skk.repository.UserRepository;
 import skk.service.UserService;
 import skk.util.FailedResponse;
@@ -12,6 +14,7 @@ import skk.util.Response;
 import skk.util.SuccessResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/mvo")
@@ -23,6 +26,9 @@ public class MvoController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BrandInfoRepository brandInfoRepository;
 
     User getUserInfo(String token){
         List<User> users = userRepository.findByToken(token);
@@ -85,6 +91,72 @@ public class MvoController {
 
         return new SuccessResponse("更新成功");
     }
+
+    @GetMapping(path = "/brandInfo")
+    public @ResponseBody
+    Response getBrandInfo(@RequestHeader(name = "x-skk-token", required = false , defaultValue = "null") String token){
+
+        User user = getUserInfo(token);
+        if(user == null){
+            FailedResponse r = new FailedResponse("身份认证失效，请重新登录");
+            return r;
+        }
+
+        List<BrandInfo> infos = brandInfoRepository.findBrandInfoByMvoId(user.id);
+
+        return new SuccessResponse(infos);
+
+    }
+
+    @PostMapping(path = "/brandInfo")
+    public @ResponseBody
+    Response postBrandInfo(@RequestHeader(name = "x-skk-token", required = false , defaultValue = "null") String token,
+                                @RequestBody BrandInfoRequest requestBody){
+
+        User user = getUserInfo(token);
+        if(user == null){
+            FailedResponse r = new FailedResponse("身份认证失效，请重新登录");
+            return r;
+        }
+        // 先检查扩展信息是否已经存在
+
+        BrandInfo info = new BrandInfo();
+
+
+        info.mvoId = user.id;
+        info.brandLogo = requestBody.brandLogo;
+        info.brandName = requestBody.brandName;
+
+        brandInfoRepository.save(info);
+
+        return new SuccessResponse("更新成功");
+    }
+
+    @DeleteMapping(path = "/brandInfo")
+    public @ResponseBody
+    Response deleteBrandInfo(@RequestHeader(name = "x-skk-token", required = false , defaultValue = "null") String token,
+                             @RequestParam(name="id") String id){
+
+        User user = getUserInfo(token);
+        if(user == null){
+            FailedResponse r = new FailedResponse("身份认证失效，请重新登录");
+            return r;
+        }
+
+        Optional<BrandInfo> brandInfo = brandInfoRepository.findById(id);
+        if(!brandInfo.isPresent()){
+            FailedResponse r = new FailedResponse("对象不存在");
+            return r;
+        }
+
+        if(!brandInfo.get().mvoId.equals(user.id)){
+            FailedResponse r = new FailedResponse("无权操作");
+            return r;
+        }
+
+        brandInfoRepository.deleteById(brandInfo.get().id);
+        return new SuccessResponse("删除成功");
+    }
 }
 
 
@@ -94,4 +166,9 @@ class BrandExtraInfoRequest {
     public String brief;
     public String gcmReportType;
     public String gcmReportUrl;
+}
+
+class BrandInfoRequest {
+    public String brandName;
+    public String brandLogo;
 }
--- server/src/main/java/skk/repository/BrandInfoRepository.java
+++ server/src/main/java/skk/repository/BrandInfoRepository.java
@@ -1,4 +1,11 @@
 package skk.repository;
 
-public interface BrandInfoRepository {
+import org.springframework.data.repository.CrudRepository;
+import skk.entity.BrandExtraInfo;
+import skk.entity.BrandInfo;
+
+import java.util.List;
+
+public interface BrandInfoRepository extends CrudRepository<BrandInfo, String> {
+    List<BrandInfo> findBrandInfoByMvoId(String mvoId);
 }
